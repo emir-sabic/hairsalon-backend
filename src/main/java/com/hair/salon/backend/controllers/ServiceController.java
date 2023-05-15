280b03d189098c301bcc6348a5b1f924d2701a4a
Belmir <belmir.zahirovic@stu.ssst.edu.ba>
2023-05-15 23:06:29 +0200

    Added unit tests for ServisService

--- src/main/java/com/hair/salon/backend/controllers/ServiceController.java
+++ src/main/java/com/hair/salon/backend/controllers/ServiceController.java
@@ -1,27 +1,50 @@
 package com.hair.salon.backend.controllers;
 
-import com.hair.salon.backend.models.ServiceDto;
+import com.hair.salon.backend.models.ServisDto;
+import com.hair.salon.backend.services.ServisService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/service")
 @RestController
 public class ServiceController {
-    @GetMapping
-    public ServiceDto getService(int ID){
-        return new ServiceDto(ID,"Imeservisa","Opisservisa",true,20);
+
+    private final ServisService servisService;
+
+    ServiceController(ServisService servisService) {
+        this.servisService = servisService;
     }
+
+    @GetMapping("/{id}/list")
+    public List<ServisDto> getServiceList(@PathVariable long id) {
+        return servisService.getServisList();
+    }
+
+
+    @GetMapping("/{id}")
+    public ServisDto getServis(@PathVariable long id) {
+        return servisService.getServis(id);
+
+    }
+
     @PostMapping
-    public boolean postService(int ID,String Name,String Desc,Boolean Avail,double Price){
-        ServiceDto x=new ServiceDto(ID,Name,Desc,Avail,Price);
-        return true;
+    public ServisDto createServis(@RequestBody ServisDto servisDto) {
+        return servisService.createServis(servisDto);
     }
-    @DeleteMapping
-    public boolean deleteService(int ID){
-        return true;
+
+    @PutMapping("/{id}")
+    public ServisDto updateServis(@PathVariable long id, @RequestBody ServisDto servisDto) {
+        return servisService.updateServis(id, servisDto);
     }
-    @PatchMapping
-    public ServiceDto patchService(int ID,String Name,String Desc,Boolean Avail,double Price){
-        ServiceDto x=new ServiceDto(ID,Name,Desc,Avail,Price);
-        return x;
+    @GetMapping
+    public List<ServisDto> getServices(){
+        List<ServisDto> result=new ArrayList<>();
+        ServisDto x=new ServisDto(10L,"Name","Desc",125);
+        result.add(x);
+        x=new ServisDto(20L,"ime","opis",50);
+        result.add(x);
+        return result;
     }
 }
---
+++ src/main/java/com/hair/salon/backend/repositories/ServisRepository.java
@@ -0,0 +1,9 @@
+package com.hair.salon.backend.repositories;
+
+
+import com.hair.salon.backend.models.entities.ServisEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServisRepository extends JpaRepository<ServisEntity, Long> {
+
+}
